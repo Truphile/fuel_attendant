5c368ab1d8eaaa97e1773ce52085b365f0ce3198
Truphile <salidmonreal@gmail.com>
2026-01-11 15:09:13 +0100

    new fuel commit

--- src/transaction.py
+++ src/transaction.py
@@ -9,3 +9,11 @@ class Transaction:
         self.liters = liters
         self.amount = amount
         self.timestamp = datetime.datetime.now()
+
+    def receipt(self) -> str :
+        return (
+                f"Fuel: {self.fuel_name}\n" 
+                f"Liters: {self.liters:.2f}L\n" 
+                f"Amount: {self.amount:.2f}\n" 
+                f"Time: {self.timestamp}"
+        )
--- test/TransactionTest.py
+++ test/TransactionTest.py
@@ -11,3 +11,12 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(tranx.liters,10.0)
         self.assertEqual(tranx.amount,7950.0)
         self.assertIsNotNone(tranx.timestamp)
+
+
+    def test_receipt_format_works(self):
+        tranx = Transaction("petrol",10.0,7950.0)
+        receipt = tranx.receipt()
+
+        self.assertIn("petrol",receipt)
+        self.assertIn("10.00L",receipt)
+        self.assertIn("7950.00",receipt)
