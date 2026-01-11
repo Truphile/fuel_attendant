a4796cefbfe9cd7d90a565a001f84ab1a1843a59
Truphile <salidmonreal@gmail.com>
2026-01-11 18:20:20 +0100

    new fuel commit

---
+++ src/fuel_attendant.py
@@ -0,0 +1,4 @@
+class FuelAttendant:
+    def __init__(self,attendant_name: str, dispenser):
+        self.attendant_name = attendant_name
+        self.dispenser = dispenser
---
+++ test/fuel_attendant_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from src.dispenser import Dispenser
+from src.fuel import Fuel
+from src.fuel_attendant import FuelAttendant
+
+
+class AttendantTest(unittest.TestCase):
+
+    def setUp(self):
+        self.dispenser = Dispenser()
+        self.dispenser.add_fuel(Fuel("Petrol", 650, 100))
+        self.attendant = FuelAttendant("Segun", self.dispenser)
